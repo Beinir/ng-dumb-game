c4862a7692ec585774d2fa0fc53eb5cef8088404
Beinir <beinir.ragnuson@Beinirs-MacBook-Pro.local>
2020-09-02 11:12:19 +0200

    Unit Tests

--- src/app/bomb/bomb.component.spec.ts
+++ src/app/bomb/bomb.component.spec.ts
@@ -19,6 +19,28 @@ describe('BombComponent', () => {
     fixture.detectChanges();
   });
 
+  it('should set show_game_win to true if game has been won', function () {
+
+    if (this.game_won == 4 && this.timer[0] != 0 && this.timer[1] != 0 && this.timer[2] != 0 && this.timer[3] != 0){
+      expect(component.show_game_win).toBe(true)
+    }
+    else {
+      expect(component.show_game_win).toBe(false)
+    }
+  });
+
+  /*it('should set game_over to true if game has been lost', function () {
+
+    if (this.timer[0] == 0.0 || this.timer[1] == 0.0 || this.timer[2] == 0.0 || this.timer[3] == 0.0 ) {
+      expect(component.game_over).toBe(true)
+    }
+    else {
+      expect(component.game_over).toBe(false)
+    }
+
+  }); */
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
